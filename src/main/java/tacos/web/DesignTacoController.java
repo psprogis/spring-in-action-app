9035acb8c5d62ce50afb730a69b746e4003e88f7
Pavel Sprogis <pavel.sprogis@gmail.com>
2019-02-17 20:44:53 +0200

    fixed typo in DesignTacoController

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -28,8 +28,8 @@ public class DesignTacoController {
         List<Ingredient> ingredients = Arrays.asList(
             new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
             new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+            new Ingredient("GRBF", "Ground Beef", Type.PROTIEN),
+            new Ingredient("CARN", "Carnitas", Type.PROTIEN),
             new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
             new Ingredient("LETC", "Lettuce", Type.VEGGIES),
             new Ingredient("CHED", "Cheddar", Type.CHEESE),

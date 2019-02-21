654d6119d2046f1301b1a122ff2f8f82b1277bf7
Pavel Sprogis <pavel.sprogis@gmail.com>
2019-02-21 17:16:21 +0200

    add WebConfig.java, remove HomeController

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+import org.springframework.validation.Errors;
+
 @Slf4j
 @Controller
 @RequestMapping("/orders")
@@ -23,7 +26,11 @@ public class OrderController {
 	}
 
 	@PostMapping
-	public String processOrder(Order order) {
+	public String processOrder(@Valid Order order, Errors errors) {
+		if (errors.hasErrors()) {
+			return "orderForm";
+		}
+		
 		log.info("order submitted");
 		
 		return "redirect:/";

f53d3d47a4bf52e9a445c85562b685ba47205c07
pavel sprogis <pavel.sprogis@gmail.com>
2019-03-20 14:45:11 +0200

    refactor to spring data jpa

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,7 +1,17 @@
 package tacos;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -11,26 +21,32 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import lombok.Data;
 
 @Data
-public class Order {
+@Entity
+@Table(name="taco_order")
+public class Order implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	
 	private Date placedAt;
 	
 	@NotBlank(message="Name is required")
-	private String name;
+	private String deliveryName;
 	
 	@NotBlank(message="Street is required")
-	private String street;
+	private String deliveryStreet;
 	
 	@NotBlank(message="City is required")
-	private String city;
+	private String deliveryCity;
 	
 	@NotBlank(message="State is required")
-	private String state;
+	private String deliveryState;
 	
 	@NotBlank(message="Zip is required")
-	private String zip;
+	private String deliveryZip;
 	
 	@CreditCardNumber(message="Not a valid credit card number")
 	private String ccNuber;
@@ -41,4 +57,15 @@ public class Order {
 	@Digits(integer=3, fraction=0, message="Invalid CVV")
 	private String ccCVV;
 	
+	@ManyToMany(targetEntity=Taco.class)
+	private List<Taco> tacos = new ArrayList<>();
+	
+	public void addDesign(Taco design) {
+	    this.tacos.add(design);
+	}
+	
+	@PrePersist
+	void placedAt() {
+	    this.placedAt = new Date();
+	}
 }
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,7 +1,6 @@
 package tacos.web;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,62 +16,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
-
 import tacos.data.IngredientRepository;
 import tacos.data.TacoRepository;
 import tacos.Order;
 
-@Slf4j
 @Controller
 @RequestMapping("/design")
 @SessionAttributes("order")
 public class DesignTacoController {
 	
-	private final IngredientRepository ingredientRepo;
-	
-	private TacoRepository designRepo;
+	private final IngredientRepository ingredientRepo;	
+	private TacoRepository tacoRepo;
 	
 	@Autowired
     public DesignTacoController(IngredientRepository ingredientRepo,
-    		TacoRepository designRepo) {
+    		TacoRepository tacoRepo) {
 	    this.ingredientRepo = ingredientRepo;
-	    this.designRepo = designRepo;
-	}
-	
-	@ModelAttribute
-	public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-            new Ingredient("CHED", "Cheddar", Type.CHEESE),
-            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-            new Ingredient("SLSA", "Salsa", Type.SAUCE),
-            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
-        
-        Type[] types = Ingredient.Type.values();
-        
-        for (Type type: types) {
-        	model.addAttribute(type.toString().toLowerCase(),
-        			filterByType(ingredients, type));
-        }
-	}
+	    this.tacoRepo = tacoRepo;
+	}	
 	
     @ModelAttribute(name = "order")
     public Order order() {
         return new Order();
     }
 	  
-    @ModelAttribute(name = "taco")
-    public Taco taco() {
+    @ModelAttribute(name = "decign")
+    public Taco design() {
         return new Taco();
     }
 	
@@ -93,14 +65,14 @@ public class DesignTacoController {
 	
 	@PostMapping
 	public String processDesign(
-        @Valid Taco design, Errors errors, 
+        @Valid Taco taco, Errors errors, 
         @ModelAttribute Order order) {
 
         if (errors.hasErrors()) {
             return "design";
         }
 
-        Taco saved = designRepo.save(design);
+        Taco saved = tacoRepo.save(taco);
         order.addDesign(saved);
 
         return "redirect:/orders/current";

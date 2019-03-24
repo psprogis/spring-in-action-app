1156a77ad78ec03ec8d4f8d48d13d3849e4a57cd
pavel sprogis <pavel.sprogis@gmail.com>
2019-03-24 18:27:49 +0200

    fix type in Order class

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -53,7 +53,7 @@ public class Order implements Serializable {
 	private String deliveryZip;
 	
 	@CreditCardNumber(message="Not a valid credit card number")
-	private String ccNuber;
+	private String ccNumber;
 	
 	@Pattern(regexp="^(0[1-9] |1[0-2]) ([\\/]) ([1-9] [0-9])$", message="Must be formatted MM/YY")
 	private String ccExpiration;

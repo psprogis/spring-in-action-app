50d5435d18027ef71e1b4bc27af6ff306292dd6a
pavel sprogis <pavel.sprogis@gmail.com>
2019-03-24 17:06:38 +0200

    update security and web configs

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -28,5 +29,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
         .userDetailsService(userDetailsService)
         .passwordEncoder(encoder());
-    }    
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                    .access("hasRole('ROLE_USER')")
+                .antMatchers("/", "/**")
+                    .access("permitAll")
+            .and()
+                .formLogin()
+                    .loginPage("/login")
+            .and()
+                .csrf()
+                    .ignoringAntMatchers("/h2-console/**")
+            .and()
+                .headers()
+                    .frameOptions()
+                        .sameOrigin();            
+    }
 }
